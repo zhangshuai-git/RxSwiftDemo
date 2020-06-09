a6d642eca48cd6703d9a5c99f31b63a2de38d34d
zhangshuai <mac.local>
2020-06-09 12:34:36 +0900

    Does not print log in release

--- RxSwiftDemo/ZSExtension/RxSwift+Extension.swift
+++ RxSwiftDemo/ZSExtension/RxSwift+Extension.swift
@@ -6,23 +6,12 @@
 //  Copyright © 2020 張帥. All rights reserved.
 //
 
-import Foundation
 import RxSwift
 
-func print<T>(_ message: T, tag: String? = nil, filePath: String = #file, methodName: String = #function, lineNumber: Int = #line) {
-    #if DEBUG
-    let formatter = DateFormatter()
-    formatter.dateFormat = "yyMMdd-HHmmss"
-    let date = formatter.string(from: Date())
-    let fileName = (filePath as NSString).lastPathComponent
-    Swift.print("\(tag ?? date) <\(fileName)> \(methodName) [Line \(lineNumber)] \(message)")
-    #endif
-}
-
 extension ObservableType {
-    public func debug(_ identifier: String? = nil, trimOutput: Bool = false, file: String = #file, line: UInt = #line, function: String = #function) -> Observable<E> {
+    public func print(_ identifier: String? = nil, trimOutput: Bool = false, file: String = #file, line: UInt = #line, function: String = #function) -> Observable<E> {
         #if DEBUG
-        return RxSwift.debug(identifier, trimOutput, file, line, function)
+        return self.debug(identifier, trimOutput: trimOutput, file: file, line: line, function: function)
         #else
         return self.asObservable()
         #endif
